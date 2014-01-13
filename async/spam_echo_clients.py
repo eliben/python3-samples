c63a3a7ce6d509f8b812472d330a499c6d0f94b0
Eli Bendersky <eliben@gmail.com>
2014-01-12 16:45:46 -0800

    adding simple client that spams connections

---
+++ async/spam_echo_clients.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+import time
+
+SERVER_HOST = 'localhost'
+SERVER_PORT = 40404
+
+sockets = []
+msg = b'first message'
+
+for i in range(20):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((SERVER_HOST, SERVER_PORT))
+    sockets.append(sock)
+    time.sleep(0.1)
+
+for sock in sockets:
+    sock.send(msg)
+    time.sleep(0.1)
+
+for sock in sockets:
+    sock.close()
+    time.sleep(0.1)
+
