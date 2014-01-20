59dfeea7dca467ebd12f3dfd14b2a672524fb224
Eli Bendersky <eliben@gmail.com>
2014-01-20 07:04:11 -0800

    Hello world Coroutine sample

---
+++ async/hello_coroutine.py
@@ -0,0 +1,10 @@
+import asyncio
+
+@asyncio.coroutine
+def greet_every_two_seconds():
+    while True:
+        print('Hello World')
+        yield from asyncio.sleep(2)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(greet_every_two_seconds())
