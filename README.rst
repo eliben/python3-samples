18d57adb8878c591eb86dfb61c0dc130a1912904
Eli Bendersky <eliben@gmail.com>
2014-01-25 10:08:06 -0800

    Adding a sample of statistics

--- README.rst
+++ README.rst
@@ -2,3 +2,7 @@ python3-samples
 ===============
 
 Samples of Python 3 code, libraries, etc.
+
+Many of the samples focus on the newest version of Python 3 (such as Python 3.4
+as of March 2014).
+
---
+++ math-and-statistics/statistics-samples.py
@@ -0,0 +1,8 @@
+import statistics
+
+d = [0.1, 0.3, 0.8, 0.8, 9.1]
+
+print('The mean is', statistics.mean(d))
+print('The median is', statistics.median(d))
+print('The standard deviation is', statistics.pstdev(d))
+
